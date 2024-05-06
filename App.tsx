451bba00f1289d46d3b764ea12ebc061385eb21d
shubham.negi <shubam.negi@debutinfotech.com>
2024-05-06 10:56:09 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+import HomeScreen from './src/screens/home_screen';
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <View>
+      <HomeScreen />
+    </View>
+  );
+};
+
+export default App;
